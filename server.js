c813716a9415956725cd35f26dbd27ff17766907
Domas <domasma@wix.com>
2020-10-17 15:09:13 +0300

    return everything

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const { ai } = require('./ai');
+const {ai} = require('./ai');
 
 function updateDatabase(scheduleNext = true) {
 
@@ -17,26 +17,30 @@ const DB = {
     address: 'Vokieciu Gatve',
     totalSpots: 100,
     takenSpots: 23,
-    updatedDate: new Date().toISOString().replace('T', ' ' ).replace('Z', '').replace(/\.\d{3}$/, ''),
+    updatedDate: new Date().toISOString().replace('T', ' ').replace('Z', '').replace(/\.\d{3}$/, ''),
     location: {lat: 54.679608, lng: 25.283881}
   },
   'vilniaus_g': {
     address: 'Vilniaus g. 39',
     totalSpots: 42,
     takenSpots: 5,
-    updatedDate: new Date().toISOString().replace('T', ' ' ).replace('Z', '').replace(/\.\d{3}$/, ''),
+    updatedDate: new Date().toISOString().replace('T', ' ').replace('Z', '').replace(/\.\d{3}$/, ''),
     location: {lat: 54.682144, lng: 25.280008}
   }
 };
 
-app.get('/street', function (req, res) {
-  console.log(req.query);
-  const street = req.query.street;
-  if (street && DB[street]) {
-    res.json(DB[street]);
-  } else {
-    res.sendStatus(404);
-  }
+app.get('/streets', function (req, res) {
+  const response = Object.entries(DB).map(([streetSlug, data]) => {
+    return {
+      address: data.address,
+      totalSpots: data.totalSpots,
+      takenSpots: data.takenSpots,
+      updatedDate: data.updatedDate,
+      location: data.location,
+    }
+  })
+
+  res.json(response);
 })
 
 app.listen(process.env.PORT || 3000);
