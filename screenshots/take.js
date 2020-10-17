b2844922b8ef8d260f9afc118719823c102093e7
Domas <domasma@wix.com>
2020-10-17 16:48:28 +0300

    screenshot #2

--- screenshots/take.js
+++ screenshots/take.js
@@ -13,20 +13,24 @@ module.exports = { takePlayAndTakeScreenshot }
 
 async function takePlayAndTakeScreenshot() {
   return new Promise(async (resolve, reject) => {
+    console.log('test1')
     let driver = await new Builder()
         .forBrowser('chrome')
-        // .setChromeOptions(
-        //     new chrome.Options().headless()
-        // )
         .build();
     try {
       await driver.sleep(1000);
+      console.log('test2')
       await driver.get('https://balticlivecam.com/cameras/lithuania/vilnius/vilnius-sv-jono-gatve/?embed');
+      console.log('test3')
       await driver.findElement(By.className('vjs-big-play-button')).click();
+      console.log('test4')
       await driver.sleep(2000);
+      console.log('test5')
       const image = await takeScreenshot(driver);
+      console.log('test6')
       resolve(image);
     } catch (e) {
+      console.error(e);
       reject(e);
     } finally {
       await driver.quit();
